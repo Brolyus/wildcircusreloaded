970ca6e6cd87437cc6d7c96e44c37946fe917896
Brolyus <desousanthony@gmail.com>
2020-01-29 15:14:26 +0100

    created router skeleton

--- front/src/App.js
+++ front/src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Link,
+	Redirect
+} from 'react-router-dom'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<div className='App'>
+				<ul>
+					<li>
+						<Link to='/home'>Home</Link>
+					</li>
+					<li>
+						<Link to='/users'>Users</Link>
+					</li>
+					<li>
+						<Link to='/events'>Events</Link>
+					</li>
+					<li>
+						<Link to='/login'>Sign In</Link>
+					</li>
+					<li>
+						<Link to='/register'>Register</Link>
+					</li>
+					<li>
+						<Link to='/events'>Say bye</Link>
+					</li>
+				</ul>
+			</div>
+
+			<Switch>
+				<Route exact path='/'>
+					<Redirect to='/home' />
+				</Route>
+				<Route path='/home'>
+					<p>Hello this is Home</p>
+				</Route>
+				<Route path='/users'>
+					<p>Hello this is Users</p>
+				</Route>
+				<Route path='/events'>
+					<p>Hello this is the events</p>
+				</Route>
+				<Route path='/register'>
+					<p>Hello this were you will register</p>
+				</Route>
+				<Route path='/login'>
+					<p>Hello this were you'll log</p>
+				</Route>
+				<Route path='/logout'>
+					<p>Hello this is were we say goodbye</p>
+				</Route>
+			</Switch>
+		</Router>
+	)
 }
 
-export default App;
+export default App
