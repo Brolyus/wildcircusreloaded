0fe47aecf6958e54a8162d4bd73e5dfa79cb584f
Brolyus <desousanthony@gmail.com>
2020-01-29 17:59:09 +0100

    added a login route, tested and work, also created some test to check if connection with DB is ok, and also testing routes

--- back/index.js
+++ back/index.js
@@ -7,8 +7,29 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get('/', (request, response) => {
-	response.send('Hello World')
+app.get('/', (req, res) => {
+	res.send('Hello World')
+})
+
+app.post('/login', (req, res) => {
+	const authData = req.body
+	if (authData.email && authData.password) {
+		connection.query(
+			'SELECT email, password FROM user WHERE email = ? AND password = ?',
+			[authData.email, authData.password],
+			(err, results) => {
+				console.log(authData)
+				console.log(results)
+				if (err) {
+					res.status(500).send('Server error 500')
+				} else if (results.length === 0) {
+					res.send('Mauvais email ou mot de passe!')
+				} else {
+					res.status(200).send("C'est good")
+				}
+			}
+		)
+	}
 })
 
 app.listen(port, err => {
