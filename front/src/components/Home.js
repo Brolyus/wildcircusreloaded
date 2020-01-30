0f682bb9e10359d3ec9a15bdf26fee9a7fdd9155
Brolyus <desousanthony@gmail.com>
2020-01-30 10:52:03 +0100

    installed react slick for carousel, it's on, now have to import data to fill it

--- front/src/components/Home.js
+++ front/src/components/Home.js
@@ -1,8 +1,22 @@
 import React from 'react'
 import '../styles/Home.css'
+import Slider from 'react-slick'
 
 class Home extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {}
+	}
+	componentDidMount() {}
 	render() {
+		const settings = {
+			className: 'center',
+			centerMode: true,
+			infinite: true,
+			centerPadding: '60px',
+			slidesToShow: 1,
+			speed: 500
+		}
 		return (
 			<div>
 				<h1>Wild Circus RELOADED</h1>
@@ -16,6 +30,29 @@ class Home extends React.Component {
 						temporibus doloribus magni odio, nostrum est totam
 						maiores vel earum.
 					</p>
+				</div>
+				<div>
+					<h2>Nos équipes</h2>
+					<Slider {...settings}>
+						<div>
+							<h3>1</h3>
+						</div>
+						<div>
+							<h3>2</h3>
+						</div>
+						<div>
+							<h3>3</h3>
+						</div>
+						<div>
+							<h3>4</h3>
+						</div>
+						<div>
+							<h3>5</h3>
+						</div>
+						<div>
+							<h3>6</h3>
+						</div>
+					</Slider>
 				</div>
 			</div>
 		)
