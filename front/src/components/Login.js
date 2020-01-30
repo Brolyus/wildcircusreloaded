3c2945e31caa8a983a6850fe3883ab3ce5684955
Brolyus <desousanthony@gmail.com>
2020-01-30 10:01:18 +0100

    created 2 basics form for login and register

--- front/src/components/Login.js
+++ front/src/components/Login.js
@@ -1,7 +1,21 @@
 import React from 'react'
 
 const Login = () => {
-	return <p>Welcome</p>
+	return (
+		<div>
+			<form>
+				<label htmlFor='email' className='space:stack'>
+					Adresse mail :
+				</label>
+				<input type='email' name='email' id='email' required />
+				<label htmlFor='password' className='space:stack'>
+					Mot de passe :
+				</label>
+				<input type='password' name='password' id='password' required />
+				<button id='submit'>Se connecter</button>
+			</form>
+		</div>
+	)
 }
 
 export default Login
