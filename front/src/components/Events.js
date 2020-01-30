2a1fb0f7017b81610ce1ae2e0a53f406e295373e
Brolyus <desousanthony@gmail.com>
2020-01-30 16:02:49 +0100

    added a new api call on Event.js

--- front/src/components/Events.js
+++ front/src/components/Events.js
@@ -1,7 +1,47 @@
 import React from 'react'
+import '../styles/Events.css'
+import axios from 'axios'
 
-const Events = () => {
-	return <p>Hello this is the events</p>
+class Events extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			firstname: '',
+			lastname: '',
+			date: '',
+			role: '',
+			qrcode: '',
+			user_id: null,
+			guest_id: null
+		}
+	}
+
+	componentDidMount() {
+		axios
+			.all([
+				axios.get('http://localhost:3000/users'),
+				axios.get('http://localhost:3000/tickets')
+			])
+			.then(
+				axios.spread((usersRes, ticketsRes) => {
+					console.log(usersRes + ' et ' + ticketsRes)
+				})
+			)
+	}
+	render() {
+		return (
+			<div>
+				<div className='profile'>
+					<h2>Mes informations</h2>
+					<p>Hello this is the events</p>
+				</div>
+				<div className='ticket'>
+					<h2>Mon ticket</h2>
+					<img src='https://media.istockphoto.com/vectors/circus-ticket-template-vector-id1162477112' />
+				</div>
+			</div>
+		)
+	}
 }
 
 export default Events
