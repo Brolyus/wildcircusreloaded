a12683e59ea1040036051222419e249af96ec4fd
Brolyus <desousanthony@gmail.com>
2020-01-30 10:32:25 +0100

    added a navbar

---
+++ front/src/components/Navbar.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import '../styles/Navbar.css'
+
+const Navbar = () => {
+	return (
+		<div>
+			<ul>
+				<li>
+					<Link to='/home'>Home</Link>
+				</li>
+				<li>
+					<Link to='/users'>Users</Link>
+				</li>
+				<li>
+					<Link to='/events'>Events</Link>
+				</li>
+				<li>
+					<Link to='/login'>Sign In</Link>
+				</li>
+				<li>
+					<Link to='/register'>Register</Link>
+				</li>
+				<li>
+					<Link to='/logout'>Say bye</Link>
+				</li>
+			</ul>
+		</div>
+	)
+}
+
+export default Navbar
